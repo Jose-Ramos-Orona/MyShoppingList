ed3599e5815ea5dd5049abe58ba90c4e5ae75534
Jose Ramos <ramosoronaj@gmail.com>
2023-03-05 12:59:29 +0100

    Add styles to App

--- src/App/AppStyled.tsx
+++ src/App/AppStyled.tsx
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 const AppStyled = styled.header`
+  .padding-large {
+    padding-top: 15px;
+  }
+
   .title {
-    font-size: 40px;
-    margin-top: 50px;
+    font-size: 1.2rem;
+    text-align: center;
     margin-bottom: 10px;
   }
 `;
