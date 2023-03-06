47bdbd40476d29870e747edefa8c59a0fa04334c
Jose Ramos <ramosoronaj@gmail.com>
2023-03-06 20:27:15 +0100

    Add providerWrapper and testing the ItemCard

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,18 @@ import { store } from "./redux/features/store";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import "papercss/dist/paper.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
